b0998da0443cb7731f29ce6ae415f7d8310d8696
刘广 <2102750146@qq.com>
2023-07-08 22:58:56 +0800

    添加项目文件。

---
+++ Project1/test.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("比特\n");
+//	return 0;
+//	//字符类型
+//	char ch = 'a';
+//	//整型
+//	int age = 20;
+//	//短整型
+//	short num = 10;
+//	//long
+//	//long long
+//	//单精度浮点型
+//	float weight = 55.5;
+//	//双精度浮点型
+//	double d = 0.0;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("比特\n");
+//	printf("%d\n", 100); //打印一个数字 - %d
+//	//sizeof - 关键字 - 操作符 - 计算类型或变量所占空间的大小
+//
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 20 ;
+//	double weight = 56.2 ;
+//	age = age + 1;
+//	weight = weight - 1;
+//	printf("%d\n", age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+//// %d - 整型
+//// %f - float
+//// %lf - double
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	//scanf函数是输入函数
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("sum = %d\n", sum);
+//	return 0;
+// }
+
+int main()
+{
+	printf();
+}
