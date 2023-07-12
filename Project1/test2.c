3e25f635d2228ff769cf2ef6608f3cc21fcb06ff
刘广 <2102750146@qq.com>
2023-07-12 16:28:16 +0800

    ...

--- Project1/test2.c
+++ Project1/test2.c
@@ -73,19 +73,31 @@
 //	}
 //}
 
-int Add(int x, int y)
-{
-	int z = 0;
-	z = x + y;
-	return z;
-}
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	//函数的方式解决
+//	int sum = Add(num1, num2);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
 int main()
 {
-	int num1 = 0;
-	int num2 = 0;
-	scanf("%d%d", &num1, &num2);
-	//函数的方式解决
-	int sum = Add(num1, num2);
-	printf("%d\n", sum);
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
 	return 0;
 }
